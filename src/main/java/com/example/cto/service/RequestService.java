5373d9542a8c270d41093e799273c2e5a19782d2
adiquee <berikka10@gmail.com>
2025-06-06 01:46:59 +0500

    (service): added service, lombok annotations and builder usage to easy write service logic

---
+++ src/main/java/com/example/cto/service/RequestService.java
@@ -0,0 +1,68 @@
+package com.example.cto.service;
+
+import com.example.cto.domain.*;
+import com.example.cto.repository.ServiceRequestRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+// logic for service requests
+@Slf4j
+@Service
+@RequiredArgsConstructor        // for generate constructors for final fields
+public class RequestService {
+
+    private final ServiceRequestRepository repository;
+
+    // create a new service request
+    public ServiceRequest createRequest(String clientName, String carModel, String description) {
+        ServiceRequest request = ServiceRequest.builder()
+                .clientName(clientName)
+                .carModel(carModel)
+                .description(description)
+                .status(RequestStatus.CREATED)
+                .build();
+
+        return repository.save(request);
+    }
+
+    // update request status
+    public ServiceRequest changeStatus(Long requestId, RequestStatus newStatus, String changedBy, String reason) {
+
+        ServiceRequest request = repository.findById(requestId)// - throws exception if request not found
+                .orElseThrow(() -> new IllegalArgumentException("Request not found: id=" + requestId));
+
+        request.setStatus(newStatus);
+
+        StatusHistory history = StatusHistory.builder()
+                .request(request)
+                .timestamp(LocalDateTime.now())
+                .changedBy(changedBy)
+                .reason(reason)
+                .newStatus(newStatus)
+                .build();
+
+        request.getHistory().add(history);
+
+        // If status is set to COMPLETED, mock the sms by log
+        if (newStatus == RequestStatus.COMPLETED) {
+            log.info("[TIPA SMS] Notify client {}: Your request is completed.", request.getClientName());
+        }
+
+        return repository.save(request);
+    }
+
+
+    // gets all requests by client name
+    public List<ServiceRequest> getByClient(String clientName) {
+        return repository.findByClientName(clientName);
+    }
+
+    // gets all requests by input status
+    public List<ServiceRequest> getByStatus(RequestStatus status) {
+        return repository.findByStatus(status);
+    }
+}
